d420d57de3fbe22bdd7a7f3236dee76de5e6d992
rmsckd1640 <rmsckd1640@gmail.com>
2026-05-29 17:31:15 +0900

    feat: HikariCP 커넥션 풀 도입

--- src/main/java/com/mini_mall/DBConnection.java
+++ src/main/java/com/mini_mall/DBConnection.java
@@ -1,57 +1,49 @@
 package com.mini_mall;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBConnection {
 
-    private static String URL;
-    private static String USER;
-    private static String PASSWORD;
+    private static final HikariDataSource dataSource;
 
     static {
-
         try {
             Properties props = new Properties();
-            InputStream input =
-                    DBConnection.class
-                            .getClassLoader()
-                            .getResourceAsStream(
-                                    "db.properties"
-                            );
+            InputStream input = DBConnection.class
+                    .getClassLoader()
+                    .getResourceAsStream("db.properties");
 
-            if(input == null) {
-                throw new RuntimeException(
-                        "db.properties 파일을 찾을 수 없습니다."
-                );
+            if (input == null) {
+                throw new RuntimeException("db.properties 파일을 찾을 수 없습니다.");
             }
 
             props.load(input);
 
-            URL = props.getProperty("db.url");
-            USER = props.getProperty("db.user");
-            PASSWORD = props.getProperty("db.password");
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(props.getProperty("db.url"));
+            config.setUsername(props.getProperty("db.user"));
+            config.setPassword(props.getProperty("db.password"));
+            
+            // 고정 풀 사이즈 설정 (최적화 권장사항 반영)
+            int poolSize = Integer.parseInt(props.getProperty("db.pool.max", "10"));
+            config.setMaximumPoolSize(poolSize);
+            config.setMinimumIdle(poolSize);
 
-            Class.forName(
-                    "com.mysql.cj.jdbc.Driver"
-            );
+            dataSource = new HikariDataSource(config);
 
-        } catch(Exception e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException("DB Connection Pool 초기화 실패", e);
         }
     }
 
-    // DB 연결 메서드
-    public static Connection getConnection()
-            throws SQLException {
-
-        return DriverManager.getConnection(
-                URL,
-                USER,
-                PASSWORD
-        );
+    // 이제 모든 DAO는 이 메서드를 통해 초고속으로 커넥션을 빌려갑니다.
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 }
